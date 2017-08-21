c03733c75043c2620869de5331acc2f619f966f9
Daniel Balck <dbalck@us.ibm.com>
2017-08-21 17:02:38 -0400

    updated startup code

--- web/webapp/apps.py
+++ web/webapp/apps.py
@@ -2,8 +2,11 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
-
+from subprocess import call
 
 class WebappConfig(AppConfig):
     name = 'webapp'
+    def ready(self):
+        call(['redis-server', '--daemonize', 'yes'])
+        #call(['celery', '-A', 'webapp', 'worker', '-l', 'info'])
 
