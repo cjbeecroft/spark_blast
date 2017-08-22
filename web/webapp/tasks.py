cd99e1242db104ab32ee1a4a06c1576d4f00a6fe
Daniel Balck <dbalck@berkeley.edu>
2017-08-21 23:35:21 -0400

    added stuff for celery

--- web/webapp/tasks.py
+++ web/webapp/tasks.py
@@ -1,37 +1,22 @@
 # Create your tasks here
 from __future__ import absolute_import, unicode_literals
-from celery import shared_task
 from celery import task
 import requests
-import uuid
 import os
 import json
 from subprocess import Popen
 from subprocess import call
 from subprocess import PIPE
 
-
-@shared_task
-def add(x, y):
-    return x + y
-
-
-@shared_task
-def mul(x, y):
-    return x * y
-
-
-@shared_task
-def xsum(numbers):
-    return sum(numbers)
-
-@task(name="submit_spark_job")
-def submit_spark_job(job_id):
+@task(name="download_data")
+def download_data(job_id):
     job = requests.get("http://localhost:8000/jobs/" + job_id + "/", auth=("daniel", "welcome1111"))
+    print "got job!"
     datasets = job.json()['datasets']
     all_files = []
     config_str = "--conf " + "dirs="
     for dataset in datasets:
+        print "iterating in datasets"
         data = requests.get(dataset, auth=("daniel", "welcome1111")).json()
 
         # if the hdfs_dir field is empty, it isn't on hdfs. we download and copy it there
@@ -61,6 +46,9 @@ def submit_spark_job(job_id):
     print config_str
 
 
+@task(name="submit_spark_job")
+def submit_spark_job(job_id):
+    job = requests.get("http://localhost:8000/jobs/" + job_id + "/", auth=("daniel", "welcome1111"))
 
     pipe = Popen(['/usr/local/spark/bin/spark-submit', '--master', 'yarn', '/home/spark/wordcount.py'], stdout=PIPE)
     stdout = pipe.communicate()[0].split("\n")
